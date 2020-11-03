2e9ed49e2e3eb44d720bde2dede1ef96d757968b
Yadnesh Kulkarni <ykulkarn@redhat.com>
2020-11-03 19:00:54 +0530

    Display appropriate message if failed to stream a song
    Signed-off-by: Yadnesh Kulkarni <ykulkarn@redhat.com>

--- beatz/player.py
+++ beatz/player.py
@@ -4,33 +4,35 @@ import sys
 import subprocess
 import re
 import time
+import shlex
 
 
 TYELLOW = '\033[33m'
 TWHITE = '\033[37m'
+TRED = '\033[31m'
 BASE_URL = "https://youtube.com"
 
 
 class Player:
-    def play_song(self, url):
-        youtube_dl_obj = youtube_dl.YoutubeDL({'quiet': True})
-        song_title = youtube_dl_obj.extract_info(url, False)['title']
+    def play_song(self, song_title, url):
         print(TYELLOW + f'  ♫ %s ' % song_title + TWHITE, end='\r')
-        cmd = 'mpv {} --really-quiet -no-video --audio-display=no'.format(url)
-        subprocess.Popen(cmd.split()).wait()
+        cmd = 'mpv --really-quiet -no-video --audio-display=no {}'.format(url)
+        proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, universal_newlines=True)
         print(f"\r    %s" % song_title)
-
+        proc.communicate()
+        if proc.returncode != 0:
+            print(TRED + f"Failed to play {song_title}" + TWHITE)
+            sys.exit(proc.returncode)
 
     def print_results(self, songs_dict):
         for song_id, song_details in songs_dict.items():
             print(f"[{song_id}] {song_details['title']} ({song_details['duration']})")
 
-
     def play_songs(self, songs_list):
         print("Pause p, Mute m, Prev <, Next >, Vol(+) *, Vol(-) /, Quit q")
         for id in songs_list:
-            self.play_song(f"{BASE_URL}{self.songs_dict[id]['url_suffix']}")
-
+            self.play_song(f"{self.songs_dict[id]['title']}", f"{BASE_URL}{self.songs_dict[id]['url_suffix']}")
 
     def play_from_search_results(self, songs_dict, max_results):
         self.songs_dict = songs_dict
